c51f2bdc4ba8ee0d84bd2e2b629327327f9c80b1
Timothy Jacobson <tim.jacobson@motixconnected.com>
2023-12-02 12:45:15 +0000

    Add gradle buildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "uk.jaconet"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("io.ktor:ktor-bom:2.3.6"))
+    implementation("io.ktor:ktor-client-core")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
