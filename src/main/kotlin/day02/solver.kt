0bbdaccc5ad0a85f61bbf1a07cdc6ced861863b6
Timothy Jacobson <tim.jacobson@motixconnected.com>
2023-12-04 12:25:34 +0000

    day02 part 2 solved

--- src/main/kotlin/day02/solver.kt
+++ src/main/kotlin/day02/solver.kt
@@ -1,6 +1,7 @@
 package day02
 
 import java.io.File
+import kotlin.math.max
 
 val example = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -41,6 +42,12 @@ data class Game(val id: Int, val results: List<Set>) {
 
     fun anySetExceedsLimit(limits: Map<Color, Int>) = results.any { it.anyScoreExceedsLimit(limits) }
 
+    val power get(): Int = results.fold(Color.entries.associateWith { 0 }) { acc, set ->
+        acc.mapValues { (color, v) ->
+            max(set.scores.singleOrNull { it.color == color }?.score ?: 0, v)
+        }
+    }.values.fold(1) { acc, res -> acc * res}
+
     companion object {
         private fun loadGame(input: String): List<Set> = input.split("; ").map(Set::loadSet)
     }
@@ -50,10 +57,10 @@ val LIMITS = mapOf(Color.RED to 12, Color.GREEN to 13, Color.BLUE to 14)
 
 fun solve(solver: (List<String>) -> Int, input: String): Int = solver(input.split("\n"))
 fun solve(input: List<String>): Int {
-    val solver = input.map {
+    val solver = input.map { line ->
         Game(
-            it.drop(5).takeWhile(Char::isDigit).toInt(),
-            it.drop(5).dropWhile { c -> c.isDigit() }.dropWhile { !it.isDigit() }
+            line.drop(5).takeWhile(Char::isDigit).toInt(),
+            line.drop(5).dropWhile { c -> c.isDigit() }.dropWhile { !it.isDigit() }
         )
     }
     return solver.sumOf { game ->
@@ -64,12 +71,19 @@ fun solve(input: List<String>): Int {
         }
     }
 }
-//
-//fun solve2(input: List<String>): Int {
-//}
+
+fun solve2(input: List<String>): Int {
+    val solver = input.map { line ->
+        Game(
+            line.drop(5).takeWhile(Char::isDigit).toInt(),
+            line.drop(5).dropWhile { c -> c.isDigit() }.dropWhile { !it.isDigit() }
+        )
+    }
+    return solver.sumOf(Game::power)
+}
 
 fun main() {
     val input =
         File("/home/timj/docs/repo/GitHub/tim-jacobson/advent-of-code-2023/src/main/resources/day02/input").readLines()
-    solve(input).also { println(it) }
+    solve2(input).also { println(it) }
 }
