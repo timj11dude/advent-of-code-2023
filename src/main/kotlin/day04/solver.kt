6e51aeecc4e1d4d2529fea09fe94f4900a4e7cd9
Timothy Jacobson <tim.jacobson@motixconnected.com>
2023-12-05 08:56:46 +0000

    day04 part 2 solved

--- src/main/kotlin/day04/solver.kt
+++ src/main/kotlin/day04/solver.kt
@@ -1,6 +1,7 @@
 package day04
 
 import java.io.File
+import java.util.Queue
 import kotlin.math.pow
 
 val example = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -11,8 +12,10 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
 data class Card(val number: Number, val winningNums: Set<Number>, val cardNums: Set<Number>) {
+    val winners: Set<Number> by lazy { winningNums intersect cardNums }
+    val numberOfWinners get() = winners.size
     val points: Int by lazy {
-        (winningNums intersect cardNums).size
+        numberOfWinners
             .takeUnless { it == 0 }
             ?.let { winners -> 2.0.pow(winners - 1).toInt() } ?: 0
     }
@@ -23,7 +26,7 @@ fun solve(input: List<String>): Int {
     val cards = input.map { line ->
         Card(
             line.dropWhile { !it.isDigit() }.takeWhile(Char::isDigit).toInt(),
-            line.drop(10).takeWhile { it != '|' }.split(" ").filterNot(String::isBlank)
+            line.dropWhile { it != ':' }.drop(1).takeWhile { it != '|' }.split(" ").filterNot(String::isBlank)
                 .map(String::toInt).toSet(),
             line.dropWhile { it != '|' }.drop(1).split(" ").filterNot(String::isBlank).map(String::toInt).toSet()
         )
@@ -31,15 +34,37 @@ fun solve(input: List<String>): Int {
     return cards.sumOf { it.points }
 }
 
-//fun solve2(input: List<String>): Int {
-//}
+fun solve2(input: List<String>): Long {
+    val cards = input.map { line ->
+        Card(
+            line.dropWhile { !it.isDigit() }.takeWhile(Char::isDigit).toInt(),
+            line.dropWhile { it != ':' }.drop(1).takeWhile { it != '|' }.split(" ").filterNot(String::isBlank)
+                .map(String::toInt).toSet(),
+            line.dropWhile { it != '|' }.drop(1).split(" ").filterNot(String::isBlank).map(String::toInt).toSet()
+        )
+    }
+    val stack = object {
+        private val list = List(10) { 0 }.toMutableList()
+        fun add(num: Int, copies: Int = 1) { list.take(num).forEachIndexed { index, i -> list[index] = i + copies } }
+        fun pop(): Int {
+            val top = list.removeFirst()
+            list.addLast(0)
+            return top
+        }
+    }
+    return cards.fold(0L) { acc, card ->
+        val copies = stack.pop()
+        stack.add(card.numberOfWinners, copies+1)
+        acc + (copies + 1)
+    }
+}
 
 fun main() {
     val input =
         File("/home/timj/docs/repo/GitHub/tim-jacobson/advent-of-code-2023/src/main/resources/day04/input").readLines()
 //    solve(::solve, example)
-    solve(input)
+//    solve(input)
 //    solve(::solve2, example)
-//    solve2(input)
+    solve2(input)
         .also { println(it) }
 }
